6b78a4d51adf0ddfbcf3855bec42c49ac7fa1552
neilauroni <neil67671@gmail.com>
2024-05-20 18:38:34 -0400

    Initial Commit

---
+++ athena Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  athena Watch App
+//
+//  Created by Neil Auroni on 5/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ athena Watch App/athenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  athenaApp.swift
+//  athena Watch App
+//
+//  Created by Neil Auroni on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct athena_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
